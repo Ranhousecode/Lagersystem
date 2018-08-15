a473475859a4f3192ec813e2dd9bd31e5cbfec4e
Daniel k <hackedbyourdad@web.de>
2018-08-16 00:04:07 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <?php
+    require_once('settings.php');
+    ?>
+    <link rel="stylesheet" type="text/css" href="/css/main.css">
+    <title>Daniel</title>
+</head>
+<body>
+        <?php
+            include('menu.php');
+        ?>
+
+<h1> Daniel</h1>
+</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,10 @@
+<table border="1">
+<tr>
+    <td>Home</td>
+    <td>Noch</td>
+    <td>Ein</td>
+    <td>Weiteres</td>
+    <td>Feld</td>
+</tr>
+</table>
+
